62ce49814d690fd485d8163666d638712c55815a
NicholasAzar <nicholas@nicholasazar.com>
2015-03-29 12:52:38 -0400

    Adding a bit of padding to menu, moving to forum, and blog.

--- components/com-networknt-light-c-blog/components/menu/menu.js
+++ components/com-networknt-light-c-blog/components/menu/menu.js
@@ -1,10 +1,29 @@
 'use strict';
 
 angular.module('lightApp').controller('menuCtrl', ['$scope', '$http', 'authService', function($scope, $http, authService) {
-    console.log("Now we are in menuCtrl");
     $scope.menuSettings = {isCollapsed : true};
     $scope.tree = [];
-
+    $scope.isUserLoggedIn = authService.authentication.currentUser.userId != '';
+    $scope.source = '';
+    /*
+    var getMenuComponent = {
+        category: 'menu',
+        name: 'getMenuComponent',
+        readOnly: true,
+        data: {
+            host: $scope.host
+        }
+    };
+    console.log('getMenuComponent posting:', getMenuComponent);
+    $http.post('api/rs', getMenuComponent)
+        .success(function(result, status, headers, config){
+            console.log('getMenuComponent success result', result);
+            $scope.source = result[0].source;
+        })
+        .error(function(result, status, headers, config){
+            console.log('getMenuComponent error result', result);
+        });
+    */
     var getMenuPost = {
         category : 'menu',
         name : 'getMenu',
@@ -16,8 +35,9 @@ angular.module('lightApp').controller('menuCtrl', ['$scope', '$http', 'authServi
 
     $http.post('api/rs', getMenuPost)
         .success(function(result, status, headers, config) {
+            console.log('getMenuPost result', result);
             $scope.tree = result.out_Own;
-            console.log("get menus", $scope.tree);
+            console.log('get menus', $scope.tree);
         });
 
     $scope.toggleCollapsed = function () {
@@ -26,7 +46,7 @@ angular.module('lightApp').controller('menuCtrl', ['$scope', '$http', 'authServi
 
     $scope.hasAccess = function(item) {
         //console.log('item = ', item);
-        //console.log('currentUser.roles', authService.authentication.currentUser.roles);
+        //console.log('currentUser', authService.authentication.currentUser);
         //console.log('item.roles', item.roles);
         for (var i = 0; i < authService.authentication.currentUser.roles.length; i++) {
             if (item.roles != null) {
@@ -44,5 +64,4 @@ angular.module('lightApp').controller('menuCtrl', ['$scope', '$http', 'authServi
         authService.logOut();
 
     };
-
 }]);
