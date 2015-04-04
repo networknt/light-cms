5a272b26cd6b4ec0948fdb2dc392fe4b14705aa9
NicholasAzar <nicholas@nicholasazar.com>
2015-04-04 18:39:32 -0400

    Synchronizing changes and removing unused components

--- components/com-networknt-light-c-menu/components/user-profile/user.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-angular.module('lightApp')
-.controller('UserCtrl', ['$scope', '$routeParams', '$http', 'toaster', 'modelDataService', 'authService', function($scope, $routeParams, $http, toaster, modelDataService, authService) {
-}]);
--- components/com-networknt-light-c-user-profile/README.md
+++
@@ -1,8 +0,0 @@
-com-networknt-light-c-user-profile
-
-Steps for integrating into light framework:
-
-1. ...
-2. ...
-
-TODO (Init: Mar 16, 2015)
