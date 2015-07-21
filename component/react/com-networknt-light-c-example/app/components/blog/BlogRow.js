f32ce88f1e1a63bd7f8682bc63ab00b245ed62c9
Nicholas Azar <nicholas@nicholasazar.com>
2015-07-20 21:12:48 -0400

    YAY! Figured out recursive tree list

--- component/react/com-networknt-light-c-example/app/components/blog/BlogRow.js
+++ component/react/com-networknt-light-c-example/app/components/blog/BlogRow.js
@@ -26,14 +26,28 @@ var BlogRow = React.createClass({
         }
 
         return (
-            <ListItem primaryText={this.props.blog.blogId} secondaryText={this.props.blog.description} onClick={this._onClick}/>
+            this._createItems(this.props.blog)
+            //<ListItem primaryText={this.props.blog.blogId} secondaryText={this.props.blog.description} onClick={this._onClick}/>
         );
     },
 
     _onClick: function() {
         console.log("Going to blog", this.props.blog);
         BlogActions.gotoBlog(this.props.blog);
+    },
+
+    _createItems: function(blogs) {
+        var children;
+        if (blogs.out_Own) {
+            children = blogs.out_Own.map(function (child) {
+                return this._createItems(child);
+            }.bind(this));
+        }
+        return (
+            <ListItem primaryText={this.props.blog.blogId} secondaryText={this.props.blog.description}>{children}</ListItem>
+        );
     }
+
 });
 
 module.exports = BlogRow;
